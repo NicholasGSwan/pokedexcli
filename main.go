4115d7deabb95cff23e9abfacc915732cb07d9c7
Nicholas Swan <nicholasgswan@gmail.com>
2026-02-19 00:41:08 -0800

    start repl loop

--- main.go
+++ main.go
@@ -1,9 +1,7 @@
 package main
 
-import (
-	"fmt"
-)
+import "github.com/NicholasGSwan/pokedexcli/repl"
 
 func main() {
-	fmt.Println("Hello, World!")
+	repl.StartRepl()
 }
--- repl/repl.go
+++ repl/repl.go
@@ -1,9 +1,25 @@
 package repl
 
-import "strings"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
 
 func cleanInput(text string) []string {
 	text = strings.ToLower(text)
 	strArr := strings.Fields(text)
 	return strArr
+}
+
+func StartRepl() {
+	scanner := bufio.NewScanner(os.Stdin)
+	for {
+		fmt.Print("Pokedex > ")
+		scanner.Scan()
+		words := cleanInput(scanner.Text())
+		fmt.Printf("Your command was: %s\n", words[0])
+
+	}
 }
